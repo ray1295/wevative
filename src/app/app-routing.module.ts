63f20a90187dbd6720f66dad4ab5ddc3c22f786e
ray1295 <ralake95@gmail.com>
2018-11-05 21:44:34 +0000

    Second commit - 1 [Set-up, Style, Filing, Pages]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WorkComponent } from './pages/work/work.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { JoinUsComponent } from './pages/join-us/join-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+  { path: 'work', component: WorkComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'join', component: JoinUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { WorkComponent } from './pages/work/work.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { JoinUsComponent } from './pages/join-us/join-us.component';
+import { NavigationComponent } from './shared/components/navigation/navigation.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkComponent,
+    BlogComponent,
+    JoinUsComponent,
+    NavigationComponent,
+    ContactComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
